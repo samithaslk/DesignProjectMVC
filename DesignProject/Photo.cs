e59329226a2c01e9ca0bc55bba92d0644f27da60
Samitha <33083074+samithaslk@users.noreply.github.com>
2019-07-05 21:09:01 +0530

    Initial commit

---
+++ DesignProject/Photo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignProject
+{
+    public class Photo
+    {
+        public int PhotoID
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public String Title
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public byte PhotoFile
+        {
+            get => default(byte);
+            set
+            {
+            }
+        }
+
+        public String Description
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public DateTime CreatedDate
+        {
+            get => default(DateTime);
+            set
+            {
+            }
+        }
+
+        public String Owner
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public List PhotoComments
+        {
+            get => default(List);
+            set
+            {
+            }
+        }
+    }
+}
